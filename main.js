70c1178443eafa150c70eafabe186580ec57e3e0
ZayRexan <mogilevkostya@yandex.ru>
2023-01-25 21:49:14 +0700

    Подключил библиотеку и переписал логику

--- main.js
+++ main.js
@@ -1,38 +1,65 @@
 let date = document.querySelector('.countdown__date');
 let form = document.querySelector('.countdown');
-let dateArr;
-let recived = {
-	year: '',
-	month: '',
-	day: '',
+let output = {
+	year: document.querySelector('.countdown__year'),
+	day: document.querySelector('.countdown__month'),
+	hour: document.querySelector('.countdown__day'),
 };
+let dateArr;
+let recived;
+
+let rest;
 
 function dateSplit() {
 	let dateStr = date.value;
-	dateArr = dateStr.split('-');
-	recived.year = dateArr[0];
-	recived.month = dateArr[1];
-	recived.day = dateArr[2];
+	recived = moment(dateStr);
+}
+
+function calculations() {
+	let now = moment();
+
+	rest = {
+		years: recived.diff(now, 'years'),
+		days: recived.diff(now, 'days'),
+		hours: recived.diff(now, 'hours'),
+	};
+
+	console.log(rest.years);
+	console.log(rest.days);
+	console.log(rest.hours);
+
+	if (rest.days >= 365) {
+		rest.days = Math.round(rest.days % 365);
+	}
+	if (rest.hours >= 24) {
+		rest.hours = Math.round(rest.hours % 24);
+	}
 }
 
 function render() {
-	let now = new Date();
-
-	let current = {
-		year: now.getFullYear(),
-		month: now.getMonth(),
-		day: now.getDate(),
-	};
-
-	let rest = {
-		year: recived.year - current.year,
-		month: recived.month - current.month,
-		day: recived.day - current.day,
-	};
+	if (rest.years <= 0 && rest.days <= 0 && rest.hours <= 0) {
+		alert('Прошлое уже не вернуть...');
+	}
+	if (rest.years > 0) {
+		output.year.innerText = `${rest.years} лет`;
+	} else {
+		output.year.innerText = '';
+	}
+	if (rest.days > 0) {
+		output.day.innerText = `${rest.days} дней`;
+	} else {
+		output.day.innerText = '';
+	}
+	if (rest.hours > 0) {
+		output.hour.innerText = `${rest.hours} часов`;
+	} else {
+		output.hour.innerText = '';
+	}
 }
 
 form.addEventListener('submit', (event) => {
 	dateSplit();
+	calculations();
 	render();
 	event.preventDefault();
 });
